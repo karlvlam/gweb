54fd72112ece19b0de6b7d38a740ff0723edaa39
Karl Lam <karl.lam@nxtomo.hk>
2015-09-18 17:46:06 +0800

    array and container study

---
+++ dynarray.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+import "container/list"
+
+type Box struct {
+    a int
+    b int
+}
+
+func main() {
+   var strSlice []string
+
+   // Append and assign to a reference
+   strSlice = append(strSlice, "One")
+   fmt.Println(strSlice)
+
+   strSlice = append(strSlice, "Two")
+   fmt.Println(strSlice)
+
+   strSlice = append(strSlice, "Three")
+   fmt.Println(strSlice)
+
+   // remove the last object of the Slice
+   strSlice = strSlice[0:len(strSlice)-1]
+   fmt.Println(strSlice)
+
+   var strList = list.New()
+   strList.PushBack("One")
+   printList(strList)
+}
+
+func printList(l *list.List) {
+    var s = ""
+    for node := l.Front(); node != nil; node = l.Next() {
+        s += node.Value.(string) + " "
+    }
+    fmt.Println(s)
+}
+
